b66841cff5141c7c75a52baaf1068b276f2067f8
MicaelaArias <micaela.arias.89@gmail.com>
2023-09-03 12:35:00 -0300

    actualizacion html

--- winesapp/urls.py
+++ winesapp/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
    path('', home, name="home"),
    path('vinos_tintos/', vinos_tintos, name= "vinos_tintos"),
    path('vinos_blancos/', vinos_blancos, name= "vinos_blancos"),
-   path('vinos_espumantes/', vinos_espumantes, name= "vinos_espumantes"),
    path('vinos_rosados/', vinos_rosados, name= "vinos_rosados"),
+   path('vinos_espumantes/', vinos_rosados, name= "vinos_espumantes"),
+    path('inicio/', inicio, name= "inicio"),
    ]
--- winesapp/views.py
+++ winesapp/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from .models import vinos_tintos
+from django.template import loader
+
+
 
 # Create your views here.
 def home(request):
     return render(request, "winesapp/home.html")
 
 def vinos_tintos(request):
-    #contexto = { 'vinos_tintos': vinos_tintos.objects.all() }
-    return render(request, "winesapp/vinos_tintos.html")   
+    contexto = { 'vinos_tintos': vinos_tintos.objects.all() }
+    return render(request, "winesapp/vinos_tintos.html", { 'vinos_tintos': vinos_tintos.objects.all() })   
 
 def vinos_blancos(request):
     return render(request, "winesapp/vinos_blancos.html")   
@@ -18,3 +21,11 @@ def vinos_espumantes(request):
 
 def vinos_rosados(request):
     return render(request, "winesapp/vinos_rosados.html")    
+
+def inicio(request):
+
+    contexto = {'vinos_tintos': vinos_tintos.objects.all()}
+    plantilla = loader.get_template("winesapp/vinos_tintos.html")
+    documento = plantilla.render( contexto )
+    
+    return HttpResponse( documento )    
