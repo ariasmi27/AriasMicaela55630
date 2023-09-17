74ea269f239925958e72a2cdf9d912c4049f39b9
MicaelaArias <micaela.arias.89@gmail.com>
2023-09-17 20:03:45 -0300

    act login logout registro

--- winesapp/views.py
+++ winesapp/views.py
@@ -11,6 +11,8 @@ from django.views.generic import ListView
 from django.views.generic import CreateView
 from django.views.generic import UpdateView
 from django.views.generic import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -19,20 +21,27 @@ from django.views.generic import DeleteView
 def home(request):
     return render(request, "winesapp/home.html")
 
+
+@login_required
 def vinos_tintos2(request):
     contexto = { 'vinos_tintos': vinos_tintos.objects.all() }
     return render(request, "winesapp/vinos_tintos.html", { 'vinos_tintos': vinos_tintos.objects.all() })   
 
+#@login_required
 def vinos_blancos2(request):
     contexto = { 'vinos_blancos': vino_blanco.objects.all() }
-    return render(request, "winesapp/vinos_blancos.html", { 'vinos_blancos' : Vino_blanco.objects.all()})   
+    return render(request, "winesapp/vinos_blancos.html", { 'vinos_blancos' : vino_blanco.objects.all()})   
 
+#@login_required
 def vinos_espumantes2(request):
-    return render(request, "winesapp/vinos_espumantes.html")  
+    contexto = {'vinos_espumantes': vino_espumante.objects.all ()}
+    return render(request, "winesapp/vinos_espumantes.html", {'vinos_espumantes': vino_espumante.objects.all()})  
 
+#@login_required
 def vinos_rosados2(request):
     return render(request, "winesapp/vinos_rosados.html")    
 
+#@login_required
 def vinos_abm(request):
     if request.method == "POST":  
         vinos = vinos_tintos(varietal=request.POST['varietal'], 
@@ -41,9 +50,11 @@ def vinos_abm(request):
         return HttpResponse("Se grabo con exito!")
     return render(request, "winesapp/vinosForm.html")    
 
+#@login_required
 def buscar_varietal(request):
     return render(request, "winesapp/buscarVarietal.html")
 
+#@login_required
 def buscar2(request):
     if request.GET['buscar']:
         patron = request.GET ['buscar']  
@@ -52,10 +63,13 @@ def buscar2(request):
         return render(request,"winesapp/vinos_tintos.html", contexto)   
     return HttpResponse ("No se ingreso nada a buscar")          
 
+#@login_required
 def vinos_blancos2(request):
     contexto = { 'vinos_blancos': vinos_blancos.objects.all() }
     return render(request, "winesapp/vinos_blancos.html", { 'vinos_blancos': vinos_blancos.objects.all() }) 
 
+
+#@login_required
 def updateVino_blanco(request, id_vino_blanco):
     vino_blanco = Vino_blanco.objects.get(id=id_vino_blanco)
     if request.method == "POST":
@@ -72,6 +86,8 @@ def updateVino_blanco(request, id_vino_blanco):
             })
     return render(request, "winesapp/vino_blancoForm.html", {'form': miForm})   
 
+
+#@login_required
 def login_request(request):
     if request.method == "POST":
         miForm = AuthenticationForm(request, data=request.POST)
@@ -91,6 +107,7 @@ def login_request(request):
 
     return render(request, "winesapp/login.html", {"form":miForm})    
 
+#@login_required
 def register(request):
     if request.method == "POST":
         miForm = RegistroUsuariosForm(request.POST)
@@ -103,19 +120,19 @@ def register(request):
     return render(request, "winesapp/registro.html", {"form":miForm}) 
 
 
-class Vino_espumanteList(ListView):
+class Vino_espumanteList(LoginRequiredMixin, ListView):
     model = vinos_espumantes
 
-class Vino_espumanteCreate(CreateView):
+class Vino_espumanteCreate(LoginRequiredMixin, CreateView):
     model = vinos_espumantes
     fields = ['varietal', 'cosecha']
-    success_url = reverse_lazy('vinos_espumantes2')
+    success_url = reverse_lazy('vinos_espumantes')
 
-class Vino_espumanteUpdate(UpdateView):
+class Vino_espumanteUpdate(LoginRequiredMixin, UpdateView):
     model = vinos_espumantes 
     fields = ['varietal', 'cosecha']
-    success_url = reverse_lazy('vinos_espumantes2')
+    success_url = reverse_lazy('vinos_espumantes')
 
-class Vino_espumanteDelete(DeleteView):
+class Vino_espumanteDelete(LoginRequiredMixin, DeleteView):
     model = vinos_espumantes
-    success_url = reverse_lazy('vinos_espumantes2')    
+    success_url = reverse_lazy('vinos_espumantes')    
